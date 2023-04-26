87e2a57b6e6fe801a668d8a7fdf0aabb0e406308
l-doppel-lucas <lucaskoeppli@hotmail.com>
2023-04-26 19:44:14 +0200

    try catch

--- woche01/script.js
+++ woche01/script.js
@@ -1,5 +1,17 @@
 console.log('test');
-alert('test');
+// alert('test');
 console.warn('test');
 console.error('test');
 console.info('test');
+
+let json = '[{"price": 675, "name": "Hans"}, {"price": 123, "name": "Peter"}]';
+
+try{ 
+    let product = JSON.parse(json);
+    if (!product[0].name){
+        throw new Error("Unvollständige Daten: Keine Produktnummer");
+    }
+    console.log(product);
+} catch (e) {
+    console.log("JSON: " + e);
+}
