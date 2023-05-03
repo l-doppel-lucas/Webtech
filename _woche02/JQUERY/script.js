e4ecc2bbf9d3de2eb0d7c13078914565c4f21ec9
l-doppel-lucas <lucaskoeppli@hotmail.com>
2023-05-03 18:52:16 +0200

    json JQUERY

---
+++ _woche02/JQUERY/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+
+    $('#test').on('click', function(){
+        console.log('Dismami');
+        $(this).html('geklickt');
+        $(this).toggleClass('green');
+    });
+
+
+    $("#btn-1").on('click', function () {
+        var txt = $('#element').text();
+        console.log(txt);
+    })
+
+    $("#btn-2").on('click', function(){
+        var txt = $("#element").html();
+        console.log(txt);
+    })
+    
+    $("#btn-3").on('click', function(){
+        var txt = $("#inputtxt").val();
+        console.log(txt);
+    })
+    
+    $("#btn-4").on('click', function(){
+        var txt = $("#element").attr("data-id");
+        console.log(txt);
+    })
+
+    //Danach einfügen
+    $("#element").after("<p>Text Nachher</p>");
+    $("#element").append("<br>Das ist weiterer Text am Schluss<br>");
+
+
+    //davor einfügen
+    $("#element").prepend("<br>Das ist weiterer Text am Anfang<br>");
+    $("#element").before("<br>noch ein Text vorher");
+
+    $("#nav").load('http://localhost/WTA/_woche02/Promise/nav.html', function(){
+        $('#test li').on('click', function(){
+            console.log('click');
+        })
+    });
+
+    $.getJSON("generated.json", function(data){
+        console.log(data);
+    });
+
+
+});
