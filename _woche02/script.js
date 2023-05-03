789bf38e9f49491d0a39718a7fbb04450ba1c32e
l-doppel-lucas <lucaskoeppli@hotmail.com>
2023-05-03 17:31:56 +0200

    Anpassung script. URL und console.log mit JSON

--- _woche02/script.js
+++ _woche02/script.js
@@ -10,3 +10,9 @@
             divPromise.innerHTML = text;
         });
     });
+
+    url = 'https://ip-api.io/json/8.8.8.8';
+
+    fetch(url).then(res => res.json()).then((data)=>{
+        console.log(data);
+    })
